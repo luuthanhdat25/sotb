47a54297e92a1291b7270084ee817c9a26a178a5
Lưu Thành Đạt <luuthanhdat19022021@gmail.com>
2023-06-13 11:08:32 +0700

    Menu UI

---
+++ Assets/Scripts/Objects/UI/Menu Scene/MenuSceneManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.Objects.UI.Menu_Scene
+{
+    public class MenuSceneManager : RepeatSceneManager
+    {
+        [SerializeField] private Transform totalUI;
+        
+        [Header("Play")]
+        [SerializeField] private Button playGameButton;
+        
+        [Header("Credit")]
+        [SerializeField] private Button creditButton;
+        [SerializeField] private Transform creditMenu;
+        [SerializeField] private Button outCreditButton;
+        
+        [Header("Exit")]
+        [SerializeField] private Button quitGameButton;
+        [SerializeField] private Transform quitMenu;
+        [SerializeField] private Button yesExitGameButton;
+        [SerializeField] private Button noExitGameButton;
+
+        //LoadComponent
+        private void Start()
+        {
+            LoadPlayButton();
+            LoadCreditButton();
+            LoadOutCreditButton();
+            LoadQuitGameButton();
+            LoadQuitMenu();
+        }
+        
+        private void LoadPlayButton()
+        {
+            playGameButton?.onClick.AddListener(NextSceneIndex);
+            playGameButton?.Select();
+        }
+        
+        private void LoadCreditButton()
+        {
+            creditButton?.onClick.AddListener(() => SetActiveTransfrom(creditMenu, true));
+            creditButton?.onClick.AddListener(CreditButtonTurnOffTotalUI);
+        }
+        
+        private void LoadOutCreditButton()
+        {
+            outCreditButton?.onClick.AddListener(() => SetActiveTransfrom(creditMenu, false));
+            outCreditButton?.onClick.AddListener(CreditButtonTurnOnTotalUI);
+        }
+        
+        private void LoadQuitGameButton()
+        {
+            quitGameButton?.onClick.AddListener(() => SetActiveTransfrom(quitMenu, true));
+            quitGameButton?.onClick.AddListener(QuitButtonTurnOffTotalUI);
+        }
+        
+        private void LoadQuitMenu()
+        {
+            yesExitGameButton?.onClick.AddListener(QuitGame);
+            noExitGameButton?.onClick.AddListener(() => SetActiveTransfrom(quitMenu, false));
+            noExitGameButton?.onClick.AddListener(QuitButtonTurnOnTotalUI);
+        }
+
+        private void CreditButtonTurnOffTotalUI()
+        {
+            SetActiveTransfrom(totalUI, false);
+            outCreditButton.Select();
+        }
+        
+        private void CreditButtonTurnOnTotalUI()
+        {
+            SetActiveTransfrom(totalUI, true);
+            playGameButton.Select();
+        }
+        
+        private void QuitButtonTurnOffTotalUI()
+        {
+            SetActiveTransfrom(totalUI, false);
+            yesExitGameButton.Select();
+        }
+        
+        private void QuitButtonTurnOnTotalUI()
+        {
+            SetActiveTransfrom(totalUI, true);
+            playGameButton.Select();
+        }
+
+        private void SetActiveTransfrom(Transform transform, bool isOn) 
+            => transform?.gameObject.SetActive(isOn);
+    }
+}
--- Assets/Scripts/Objects/UI/RepeatSceneManager.cs
+++ Assets/Scripts/Objects/UI/RepeatSceneManager.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RepeatSceneManager : MonoBehaviour
+public class RepeatSceneManager : RepeatMonoBehaviour
 {
-    public void NextScene()
+    public virtual void NextSceneIndex()
+        => LoadSceneByIndex(SceneManager.GetActiveScene().buildIndex + 1);
+    
+    public virtual void LoadSceneByName(string sceneName) 
+        => UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+    public virtual void LoadSceneByIndex(int sceneIndex)
     {
-        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+        if (sceneIndex < 0 || sceneIndex >= UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.LogError("Invalid scene index.");
+            return;
+        }
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
     }
+    
+    public virtual void ReloadScene() 
+        => UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+
+    public virtual void QuitGame() => Application.Quit();
 }
